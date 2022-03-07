e23c16033ddf706fd5db77bc2532148f0e093ddb
MaurixDLOL <may.power@hotmail.com>
2022-03-07 04:14:10 -0300

    Primer commit

---
+++ helpers/multiplicar.js
@@ -0,0 +1,45 @@
+
+
+const fs = require("fs");
+
+const colors = require("colors");
+
+const crearArchivo = async (base = 5, listar = false, hasta = 10) => {
+
+    try{
+
+        let salida = "";
+        let consola = "";
+
+        for(let i = 1; i <=hasta; i++){
+    
+            salida += `${base} "x" ${i} = ${base * i}\n`;
+            consola += `${base} ${"x".brightBlue} ${i} = ${base * i}\n`;
+        }
+
+        if(listar){
+
+            console.log("=========".green);
+            console.log(("Tabla del:",colors.cyan(base)));
+            console.log("==========".green);
+
+            console.log(consola);
+
+        }
+
+    
+        fs.writeFileSync(`./salida/tabla-${base}.txt`, salida)
+    
+        return `tabla-${base}.txt`.rainbow;
+        
+    }catch(err){
+        throw err
+    }
+
+   
+
+}
+
+module.exports = {
+    crearArchivo
+}
